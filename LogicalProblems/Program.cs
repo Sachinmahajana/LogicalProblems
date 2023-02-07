d001f20d9e9fa51924fc339c34423ab20adfe531
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-08 00:49:25 +0530

    [Sachin] Modified the program

--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -12,9 +12,19 @@ namespace LogicalProblems
         {
             
             Console.WriteLine("Welcome to the Logical Programs");
+            Console.WriteLine("\n1.Primenumber");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                // Q1 PrimeNumber
+                case 1:
+                    Primenumber myObj1 = new Primenumber();
+                    myObj1.FindPrimeNumber();
+                    break;
+            }
            
-            Primenumber myObj1 = new Primenumber();
-            myObj1.FindPrimeNumber();
+            
             Console.ReadLine();
         }
     }
