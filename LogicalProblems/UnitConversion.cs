45ac8fb8a4363c637dfc571edae56d9c71942687
Sachinmahajana <sachinmahajan782@gmail.com>
2023-02-09 01:01:40 +0530

    [Sachin] Adding UnitConversion

---
+++ LogicalProblems/UnitConversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class UnitConversion
+    {
+        public void UnitConversionFun()
+        {
+            // Celsius to Fahrenheit
+            Console.WriteLine("Enter a temprature in Celsius  : ");
+            float valInCelsius = Convert.ToInt32(Console.ReadLine());
+            float celToFahren = ((valInCelsius * 9 / 5) + 32);
+            Console.WriteLine("temp in fahrenheit :" + celToFahren);
+            // Fahrenheit to Celsius
+            Console.WriteLine("Enter a temprature in Fahrenheit : ");
+            float valInFahren = Convert.ToSingle(Console.ReadLine());
+            float fahToCelsius = (valInFahren - 32) * (5 / (float)9);
+            Console.WriteLine("temp in Celsius :" + fahToCelsius);
+        }
+    }
+}
